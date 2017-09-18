e76c7afb23688f3ee73781ca0d64fc08ded293bb
NgoTuongVi <ngovi996@gmail.com>
2017-09-18 14:53:45 +0700

    FFF215

---
+++ BTTH3/XL_BANG.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BTTH3
+{
+    class XL_BANG: DataTable()
+    {
+  //danh sach bien cuc bo
+        #region Bien cuc bo
+            public static String Chuoi_lien_ket;
+            private SqlDataAdapter mBo_doc_ghi = new SqlDataAdapter();
+            private SqlConnection mKet_noi;
+            public string chuoi_csdl;
+            private string mChuoi_sql;
+            private string mTen_bang;
+        #endregion
+
+    
+//danh sach thuoc tinh
+        #region Cac thuoc tinh
+            public String Chuoi_SQL
+            {
+                get { return mChuoi_sql; }
+                set { mChuoi_sql = value;}
+             }
+             public String Ten_bang
+            {
+                get { return mTen_bang; }
+                set { mTen_bang = value;}
+             }
+             public int So_dong
+            {
+                get { return this.DefaultView.Count; }
+              
+             }
+          #endregion
+    }
+
+  //cac phuong thuc khoi tao
+        #region Cac phuong thuc khoi tao
+                public XL_BANG (): base(){}
+            //tao moi bang
+        public XL_BANG(String pTen_bang)
+
+}
